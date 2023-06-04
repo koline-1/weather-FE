169b0218f44f56e35db8026fea2681ff41201e11
koline-1 <dnjsdn4580@gmail.com>
2023-06-04 11:04:07 +0900

    mainpage get data directly from 기상청 api

--- src/components/ServiceList.js
+++ src/components/ServiceList.js
@@ -1,4 +1,3 @@
-import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { PropTypes } from 'prop-types';
 import styles from '../styles/common/Common.module.css';
