93e2f937916b13d58139dcfec7048314468d4940
koline-1 <dnjsdn4580@gmail.com>
2023-06-12 16:41:08 +0900

    remove unnecessary codes

--- src/hooks/useList.js
+++ src/hooks/useList.js
@@ -5,14 +5,14 @@ export default function useList(path, serviceId, page, locationCode, nxValue, ny
 
     const url = `http://localhost:8080/${path}-term/${serviceId}/list?size=15&page=${Number(page)-1}`;
     const sub_url = locationCode || (nxValue && nyValue) ? path === 'mid' ? `&location=${locationCode}` : `&nxValue=${nxValue}&nyValue=${nyValue}` : '';
-console.log(nxValue, nyValue);
+
     const getData = async() => {
         return await (await fetch(url + sub_url)).json();
     }
 
     const keys = [path, serviceId, page, locationCode, nxValue, nyValue, "list"];
     const queryKey = keys.filter((item) => item);
-console.log(queryKey)
+
     const { data, isLoading, refetch } = useQuery(queryKey, getData);
 
     return { data, isLoading, refetch }
